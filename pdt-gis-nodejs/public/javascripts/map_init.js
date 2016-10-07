10228357fa88a47333146ba8d8d4afb80fcada70
Andrej Mlyncar <a.mlyncar@gmail.com>
2016-10-07 20:24:17 +0200

    Added map and sidebar dashboard.

---
+++ pdt-gis-nodejs/public/javascripts/map_init.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW1seW5jYXIiLCJhIjoiY2l0enZ0M2k0MDA0dDNubjI0cm45dDZ6ciJ9.nHZ1ZuSLr65j2WD0cVW1ag';
+console.log("Initializing map container");
+var map = new mapboxgl.Map({
+    container: 'map-id',
+    style: 'mapbox://styles/mapbox/dark-v9',
+    hash: true,
+    center: [18.12, 45.79],
+    zoom: 3
+});
+console.log("Map container initialized");
